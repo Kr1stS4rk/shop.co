6f31c5577bda981d51d43fb9e2bff0ee91e110ff
Kristina <kristsarkisyan@mail.ru>
2026-05-07 15:24:15 +0300

    changes

--- asserts/template/js/main.js
+++ asserts/template/js/main.js
@@ -4,3 +4,58 @@ const closeBtn = topBanner.querySelector('.top-banner__close');
 closeBtn.addEventListener('click', () => {
   topBanner.classList.add('top-banner--hidden');
 });
+const ratingBlocks = document.querySelectorAll(".product__rating");
+
+ratingBlocks.forEach(function (ratingBlock) {
+  const ratingValue = Number(ratingBlock.dataset.rating);
+  const starsContainer = ratingBlock.querySelector(".product__stars");
+  const ratingCurrent = ratingBlock.querySelector(".product__rating-current");
+
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("span");
+    star.classList.add("star");
+
+    const starFill = document.createElement("span");
+    starFill.classList.add("star-fill");
+
+    if (ratingValue >= i) {
+      starFill.style.width = "100%";
+    } else if (ratingValue > i - 1) {
+      starFill.style.width = (ratingValue - (i - 1)) * 100 + "%";
+    } else {
+      starFill.style.width = "0%";
+    }
+
+    star.appendChild(starFill);
+    starsContainer.appendChild(star);
+  }
+
+  ratingCurrent.textContent = ratingValue.toFixed(1);
+});
+    const productCards = document.querySelectorAll(".product__card");
+
+productCards.forEach(function (card) {
+  const price = card.dataset.price;
+  const oldPrice = card.dataset.oldPrice;
+  const discount = card.dataset.discount;
+
+  const currentPriceElement = card.querySelector(".product__price-current");
+  const oldPriceElement = card.querySelector(".product__price-old");
+  const discountElement = card.querySelector(".product__price-discount");
+
+  if (price) {
+    currentPriceElement.textContent = `$${price}`;
+  }
+
+  if (oldPrice) {
+    oldPriceElement.textContent = `$${oldPrice}`;
+  } else {
+    oldPriceElement.remove();
+  }
+
+  if (discount) {
+    discountElement.textContent = `-${discount}%`;
+  } else {
+    discountElement.remove();
+  }
+});
